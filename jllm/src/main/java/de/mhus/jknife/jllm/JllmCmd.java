2e7100bb0e6cc8882b1610b7923c3c3f2b9696c4
Mike Hummel <mike@mhus.de>
2026-09-11 08:11:27 +0200

    release: v0.3.0

--- jllm/src/main/java/de/mhus/jknife/jllm/JllmCmd.java
+++ jllm/src/main/java/de/mhus/jknife/jllm/JllmCmd.java
@@ -33,7 +33,7 @@ import java.util.concurrent.Callable;
 public class JllmCmd implements Callable<Integer> {
 
     /** keep in sync with the maven project version */
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.3.0";
 
     @Override
     public Integer call() {
